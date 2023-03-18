5658af963d378ce45a63fb4ccd9163837c5918c0
edswafford <edswafford@mchsi.com>
2023-03-18 10:41:02 -0500

    test failure

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -22,7 +22,7 @@ async fn health_check_works() {
 
     // Assert
     assert!(response.status().is_success());
-    assert_eq!(Some(0), response.content_length());
+    assert_eq!(Some(1), response.content_length());
 }
 
 // No .await call, therefore no need for `spawn_app` to be async now.
